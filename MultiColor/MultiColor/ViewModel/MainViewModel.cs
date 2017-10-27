ced43dcf8ce63427849cfa1ab557463c9afa4ab5
ing.joel2508 joel <xtudia.ing.joelarias@hotmail.com>
2017-10-27 12:19:48 -0700

    Proyect in function

---
+++ MultiColor/MultiColor/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiColor.ViewModel
+{
+    public class MainViewModel
+    {
+        public SliderColorViewModel SliderColor { get; set; }
+        public MainViewModel()
+        {
+            SliderColor = new SliderColorViewModel();
+        }
+    }
+}
